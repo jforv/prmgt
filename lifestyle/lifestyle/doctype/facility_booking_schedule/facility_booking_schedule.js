57b66bad45224c4e4fe221d2542ca01e751223a4
jforv <xbrain1@gmail.com>
2018-12-14 15:39:20 -0400

    Doctype created

---
+++ lifestyle/lifestyle/doctype/facility_booking_schedule/facility_booking_schedule.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2018, Verynice SRL and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Facility Booking Schedule', {
+	refresh: function(frm) {
+
+	}
+});
